5a50ee845ace2f515d6ed0358a4279fc8c20c355
Juan Carlos Cardoso de Oliveira <juanplaystation82@gmail.com>
2018-04-20 00:14:27 -0300

    Mais algumas mudanças que eu não sei descrever

--- ControleEstoque/arquivo.h
+++ ControleEstoque/arquivo.h
@@ -1,21 +1,21 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef ARQUIVO_H
+#define ARQUIVO_H
 #define SIZE 100
 
-// Declaração da estrutura dos registros.
+// DeclaraÃ§Ã£o da estrutura dos registros.
 typedef struct{
     char nome[SIZE];
     float precoVenda;
     int idade;
     char pais[SIZE];
-    int codProduto
+    int codProduto;
     int quantidadeEstoque;
 }REGISTRO;
 
-// Declaração da lista de registros.
+// DeclaraÃ§Ã£o da lista de registros.
 typedef struct{
     REGISTRO A[SIZE];
     int numElemento;
 }LISTA;
 
-#endif
+#endif // ARQUIVO_H
